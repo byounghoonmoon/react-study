7d87b87239556e41834eeca2bbbdf544d009ebe2
ByoungHoon Moon <passion.mbh@gmail.com>
2021-04-10 17:37:00 +0900

    React - Create concat & shouldComponentUpdate

--- src/App.js
+++ src/App.js
@@ -49,11 +49,12 @@ class App extends Component {
           onSubmit={function (_title, _desc) {
             
             this.max_content_id = this.max_content_id  + 1;
-            this.state.contents.push(
-              {id:this.max_content_id, title:_title, desc:_desc}
-            );
+            // this.state.contents.push(
+            //   {id:this.max_content_id, title:_title, desc:_desc}
+            // );
+            var _contents = this.state.contents.concat({id:this.max_content_id, title:_title, desc:_desc});
             this.setState(
-              {contents:this.state.contents}
+              {contents:_contents}
             );
           }.bind(this)}
         ></CreateContent>
