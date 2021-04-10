066cb850f72febb0220dc9083077094c70b6a0c7
ByoungHoon Moon <passion.mbh@gmail.com>
2021-04-10 17:38:24 +0900

    React - Component 명칭 수정

--- src/components/CreateContent.js
+++ src/components/CreateContent.js
@@ -1,23 +1,14 @@
 import React, { Component } from "react";
 
-class ReadContent extends Component {
+class CreateContent extends Component {
   render() {
     return (
       <article>
-        <h2>CREATE</h2>
-        <form action="/create_process" method="post"
-          onSubmit={function(e){
-            e.preventDefault();
-            this.props.onSubmit(e.target.title.value, e.target.desc.value);
-          }.bind(this)}
-        >
-          <p><input type="text" name="title" placeholder="title"></input></p>
-          <p><textarea name="desc" placeholder="description"></textarea></p>
-          <p><input type="submit"></input></p>
-        </form>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
       </article>
     );
   }
 }
 
-export default ReadContent;
+export default CreateContent;
