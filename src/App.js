f1c53ce51a4a9060de5d9eeeb8e54293f57d1ca9
ByoungHoon Moon <passion.mbh@gmail.com>
2021-04-10 12:47:41 +0900

    React - 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
-import React, {Component} from 'react'
-import './App.css';
+import React, { Component } from "react";
+import Subject from "./components/Subject";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import "./App.css";
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+  render() {
+    return (
       <div className="App">
-        <header>
-        <h1>WEB</h1>
-        world wide web!
-      </header>
-      
-      <nav>
-        <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-        </ul>        
-      </nav>
-
-      <article>
-        <h2>HTML</h2>
-        HTML is HyperText Markup Language.
-      </article>
-      
+        <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
       </div>
-    );    
+    );
   }
 }
 
