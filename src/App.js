bb4db5c7da4bcbf0d92c1825544d7dc3aa99bc74
ByoungHoon Moon <passion.mbh@gmail.com>
2021-04-10 14:43:07 +0900

    React - CRUD 초기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Subject from "./components/Subject";
 import TOC from "./components/TOC";
 import Content from "./components/Content";
+import Control from "./components/Control";
 import "./App.css";
 
 class App extends Component {
@@ -54,8 +55,10 @@ class App extends Component {
             this.setState({mode:'read', selected_content_id:Number(id)});
           }.bind(this)}
         >
-
         </TOC>
+        <Control onChangeMode={function(_mode){
+          this.setState({mode:_mode})
+        }.bind(this)}></Control>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
